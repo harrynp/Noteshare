5192b38ce58f5043444a3f1ee674a9940101e68a
Viral Shah <Viral_Shah@Virals-MacBook-Pro.local>
2015-05-06 21:29:19 -0700

    set new database

--- web/php_includes/db_conx.php
+++ web/php_includes/db_conx.php
@@ -1,5 +1,15 @@
 <?php
-$db_conx = mysqli_connect("localhost", "root", "", "social");
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+//$conn = new mysqli($server, $username, $password, $db);
+//$db_conx = mysqli_connect("localhost", "root", "", "social");
+$db_conx = mysqli_connect($server, $username, $password, $db);
 // Evaluate the connection
 // if (mysqli_connect_errno()) {
 //     echo mysqli_connect_error();
