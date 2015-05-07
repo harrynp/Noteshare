bf182ff57ee5d20e2314844e101a927cd441ade3
Viral Shah <Viral_Shah@Virals-MacBook-Pro.local>
2015-05-06 22:08:06 -0700

    think redirect is fixed

--- web/user.php
+++ web/user.php
@@ -14,7 +14,7 @@ $lastsession = "";
 if(isset($_GET["u"])){
 	$u = preg_replace('#[^a-z0-9]#i', '', $_GET['u']);
 } else {
-    header("location: http://www.friendster.com");
+    header("location: #");
     exit();
 }
 // Select the member from the users table
