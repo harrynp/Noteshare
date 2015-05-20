0d05848aef69e85784d5e841b2a572302445ff7e
Viral Shah <Viral_Shah@dhcp-v058-110.mobile.uci.edu>
2015-05-20 16:18:12 -0700

    reformat tables

--- web/make_my_tables.php
+++ web/make_my_tables.php
@@ -5,7 +5,8 @@ $tbl_users = "CREATE TABLE IF NOT EXISTS users (
               id INT(11) NOT NULL AUTO_INCREMENT,
 			  username VARCHAR(16) NOT NULL,
 			  email VARCHAR(255) NOT NULL,
-			  password VARCHAR(255) NOT NULL,
+			  password_hash VARCHAR(255) NOT NULL,
+              salt VARCHAR(255) NOT NULL,
 			  gender ENUM('m','f') NOT NULL,
 			  website VARCHAR(255) NULL,
 			  country VARCHAR(255) NULL,
@@ -93,7 +94,7 @@ $tbl_photos = "CREATE TABLE IF NOT EXISTS photos (
                 id INT(11) NOT NULL AUTO_INCREMENT,
                 user VARCHAR(16) NOT NULL,
                 gallery VARCHAR(16) NOT NULL,
-				filename VARCHAR(255) NOT NULL,
+				url VARCHAR(255) NOT NULL,
                 description VARCHAR(255) NULL,
                 uploaddate DATETIME NOT NULL,
                 PRIMARY KEY (id)
