d1d98843a460358dae12081d62ce5c824c7dc3d6
Harry <harrynp12@gmail.com>
2015-05-20 17:57:32 -0700

    format change

--- web/php_parsers/photomanager.php
+++ web/php_parsers/photomanager.php
@@ -1,5 +1,6 @@
 <?php
-require '../vendor/autoload.php';
+include_once("../php_includes/check_login_status.php");
+require ()'../vendor/autoload.php');
 $s3  = Aws\S3\S3Client::factory();
 $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
 if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
@@ -9,4 +10,4 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES
         $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
         }
     catch(Exception $e) {}
-      ?> 
+?>
