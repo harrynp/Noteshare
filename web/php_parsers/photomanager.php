8f5903328b938ca6e7faccaf1469cfe6aae15bd7
Harry <harrynp12@gmail.com>
2015-05-23 18:04:51 -0700

    added header '#'

--- web/php_parsers/photomanager.php
+++ web/php_parsers/photomanager.php
@@ -22,6 +22,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES
     $sql = "UPDATE users SET avatar='$url' WHERE username='$log_username' LIMIT 1";
     $query = mysqli_query($db_conx, $sql);
     mysqli_close($db_conx);
+    header("#");
     exit();
   }
 ?>
