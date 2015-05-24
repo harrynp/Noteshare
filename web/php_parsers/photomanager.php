2ae3fd5c785e18cb18a27c4e35f32444a65f0e3a
Harry <harrynp12@gmail.com>
2015-05-23 17:51:28 -0700

    changed ../vendor to ../../vendor

--- web/php_parsers/photomanager.php
+++ web/php_parsers/photomanager.php
@@ -2,7 +2,7 @@
 session_start();
 include_once("php_includes/db_conx.php");
 include_once("php_includes/check_login_status.php");
-require ('../vendor/autoload.php');// this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
+require ('../../vendor/autoload.php');// this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
 
 $s3 = Aws\S3\S3Client::factory();
 $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
