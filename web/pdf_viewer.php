1d964c5fddccfc770c009a0a262b86a07662fe72
Harry <harrynp12@gmail.com>
2015-05-26 18:06:58 -0700

    pregreplace test

--- web/pdf_viewer.php
+++ web/pdf_viewer.php
@@ -2,11 +2,13 @@
 $url = "";
 if(isset($_GET["url"])){
 	$url = $_GET["url"];
+  $url = preg_replace('https://', '', $_GET['url']);
+
 } else {
     header("location: http://secure-savannah-9905.herokuapp.com/");
     exit();
 }
-$pdf_viewer = '<embed src="'.rawurlencode($url).'" type="application/pdf" />';
+$pdf_viewer = '<embed src="https://'.urlencode($url).'" type="application/pdf" />';
 // $pdf_viewer =<iframe src="http://docs.google.com/gview?url=http://domain.com/your_pdf.pdf&embedded=true" style="width:600px; height:500px;" frameborder="0"></iframe>
 $pdf_viewer .= $url;
 ?>
