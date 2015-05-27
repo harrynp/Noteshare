720fbcdb540c75de6dbd1e6211c3d21831de36d4
Harry <harrynp12@gmail.com>
2015-05-26 17:11:49 -0700

    testing pdf viewer

--- web/pdf_viewer.php
+++ web/pdf_viewer.php
@@ -1,6 +1,13 @@
 <?php
-$file = $_GET['f'];
-$pdf_viewer = '<object data=".'$file'."></object>';
+$f = "";
+if(isset($_GET["f"])){
+	$u = $_GET["f"];
+} else {
+    header("location: http://secure-savannah-9905.herokuapp.com/");
+    exit();
+}
+$f = $_GET['f'];
+$pdf_viewer = '<object data=".'$f'."></object>';
 ?>
 
 <!DOCTYPE html>
