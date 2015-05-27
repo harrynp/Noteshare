1fb4d32ba82724a2f6a801ae9c8b15cc9ab94a09
Viral Shah <Viral_Shah@dhcp-v057-029.mobile.uci.edu>
2015-05-27 14:27:39 -0700

    background3

--- web/index.php
+++ web/index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 <?php include_once("template_pageTop.php"); ?>
-<div id="pageMiddle" style="background-image: url('image/background.jpg'); background-size: cover, auto;">
+<div id="pageMiddle" style="background-image: url('images/background.jpg'); background-size: cover, auto;">
 </div>
 <?php include_once("template_pageBottom.php"); ?>
 </body>
