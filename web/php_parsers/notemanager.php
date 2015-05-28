94f5ef11d36bc4d50958357fd62657ba1026ab0e
Harry <harrynp12@gmail.com>
2015-05-28 14:50:54 -0700

    changed name of notes uploaded to s3

--- web/php_parsers/notemanager.php
+++ web/php_parsers/notemanager.php
@@ -14,7 +14,8 @@ if($user_ok != true || $log_username == "") {
 if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
     // FIXME: add more validation, e.g. using ext/fileinfo
     // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
-    $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
+		$note_name = $_Post['note_name'];
+    $upload = $s3->upload($bucket, $note_name.'.pdf', fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
     $class = $_POST['class'];
 
     $url = $upload->get('ObjectURL');
