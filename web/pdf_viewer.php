5f36a5408a1a4b19eda831f6cf55f7a49fa10c17
Harry <harrynp12@gmail.com>
2015-05-28 15:21:18 -0700

    testing embed pdf viewer

--- web/pdf_viewer.php
+++ web/pdf_viewer.php
@@ -10,7 +10,8 @@ if(isset($_GET["url"])){
     header("location: http://secure-savannah-9905.herokuapp.com/");
     exit();
 }
-$pdf_viewer = '<embed src="'.$url.'" type="application/pdf"></object>';
+// $pdf_viewer = '<embed src="'.$url.'" type="application/pdf"></object>';
+$pdf_viewer = '<iframe src="http://docs.google.com/gview?url='.$url.'" style="width:600px; height:500px;" frameborder="0"></iframe>'
 // $pdf_viewer =<iframe src="http://docs.google.com/gview?url=http://domain.com/your_pdf.pdf&embedded=true" style="width:600px; height:500px;" frameborder="0"></iframe>
 $pdf_viewer .= $url;
 ?>
