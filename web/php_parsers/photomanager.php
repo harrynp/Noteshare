1c1c056fdac080b04627a73ec9346f75811438fd
Viral Shah <Viral_Shah@dhcp-v045-204.mobile.uci.edu>
2015-05-28 14:23:22 -0700

    sample

--- web/php_parsers/photomanager.php
+++ web/php_parsers/photomanager.php
@@ -14,7 +14,7 @@ if($user_ok != true || $log_username == "") {
 if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
     // FIXME: add more validation, e.g. using ext/fileinfo
     // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
-    $upload = $s3->upload($bucket, $_FILES['userfile'][$log_username], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
+    $upload = $s3->upload($bucket, $_FILES['userfile'][''.$log_username], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
 
     $url = $upload->get('ObjectURL');
 
