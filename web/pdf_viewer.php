0ead559fe2c9b81051e30a965c9c92aeac5fc735
Viral Shah <Viral_Shah@dhcp-v045-204.mobile.uci.edu>
2015-05-28 15:50:51 -0700

    vdjvhfjkvnfjvnfjkv

--- web/pdf_viewer.php
+++ web/pdf_viewer.php
@@ -11,7 +11,7 @@ if(isset($_GET["url"])){
     exit();
 }
 // $pdf_viewer = '<embed src="'.$url.'" type="application/pdf"></object>';
-$pdf_viewer = '<iframe src="https://docs.google.com/gview?url='.$url.'&embedded=true" style="width:100%; height:100%;" frameborder="0"></iframe>';
+$pdf_viewer = '<iframe src="https://docs.google.com/gview?url='.$url.'&embedded=true" style="width:100%; height:875px;" frameborder="0"></iframe>';
 ?>
 
 <!DOCTYPE html>
