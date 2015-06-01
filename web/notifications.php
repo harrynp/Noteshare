ee6b4f19ad5c5922aa7a9403cdb87f176c11dbd6
Harry <harrynp12@gmail.com>
2015-06-01 11:48:44 -0700

    testing avatar pic on notification

--- web/notifications.php
+++ web/notifications.php
@@ -19,7 +19,14 @@ if($numrows < 1){
 		$note = $row["note"];
 		$date_time = $row["date_time"];
 		$date_time = strftime("%b %d, %Y", strtotime($date_time));
-		$notification_list .= "<p><a href='user.php?u=$initiator'>$initiator</a> | $app<br />$note</p>";
+		$thumbquery = mysqli_query($db_conx, "SELECT avatar FROM users WHERE username='$user1' LIMIT 1");
+		$thumbrow = mysqli_fetch_row($thumbquery);
+		$user1avatar = $thumbrow[0];
+		$user1pic = '<img src="'.$user1avatar.'" alt="'.$user1.'" class="user_pic">';
+		if($user1avatar == NULL){
+			$user1pic = '<img src="images/avatardefault.gif" alt="'.$user1.'" class="user_pic">';
+		}
+		$notification_list .= "<p><img src=$user1pic alt=\"\"><a href='user.php?u=$initiator'>$initiator</a> | $app<br />$note</p>";
 	}
 }
 mysqli_query($db_conx, "UPDATE users SET notescheck=now() WHERE username='$log_username' LIMIT 1");
