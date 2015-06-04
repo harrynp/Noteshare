e807a7375a20b0f6789b62a7b717b5264c41b8da
Harry <harrynp12@gmail.com>
2015-06-04 14:25:07 -0700

    fixing forgot password

--- web/forgot_pass.php
+++ web/forgot_pass.php
@@ -41,24 +41,7 @@ if(isset($_POST["e"])){
 			}
 			echo 'Email sent.  Please check your inbox at.'. $e . '.';
 			exit();
-		//$to = "$e";
-		//$from = "no-reply@friendster.com";
-		//$headers ="From: $from\n";
-		//$headers .= "MIME-Version: 1.0\n";
-		//$headers .= "Content-type: text/html; charset=iso-8859-1 \n";
-		//$subject ="Friendster Temporary Password";
-		//$msg = '<h2>Hello '.$u.'</h2><p>This is an automated message from NoteShare. If you did not recently initiate the Forgot Password process, please disregard this email.</p><p>You indicated that you forgot your login password. We can generate a temporary password for you to log in with, then once logged in you can change your password to anything you like.</p><p>After you click the link below your password to login will be:<br /><b>'.$tempPass.'</b></p><p><a href="http://www.friendster.com/forgot_pass.php?u='.$u.'&p='.$hashTempPass.'">Click here now to apply the temporary password shown below to your account</a></p><p>If you do not click the link in this email, no changes will be made to your account. In order to set your login password to the temporary password you must click the link above.</p>';
-		//if(mail($to,$subject,$msg,$headers)) {
-		//	echo "success";
-		//	exit();
-		//} else {
-		//	echo "email_send_failed";
-		//	exit();
-		//}
-    //} else {
-    //    echo "no_exist";
-    //}
-    //exit();
+		}
 }
 ?><?php
 // EMAIL LINK CLICK CALLS THIS CODE TO EXECUTE
