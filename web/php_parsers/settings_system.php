4b797e6a4ed233ff37765e9f29ee32ec8d6cbf9c
Harry <harrynp12@gmail.com>
2015-06-04 18:05:54 -0700

    fixing setting

--- web/php_parsers/settings_system.php
+++ web/php_parsers/settings_system.php
@@ -11,7 +11,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $p = $_POST['password'];
     $salt = crypt($p);
     $p_hash = hash('sha256', $p.$salt);
-    $sql = "UPDATE users SET password_hash='$p_hash' AND salt='$salt' WHERE username='$log_username' LIMIT 1";
+    $sql = "UPDATE users SET password_hash='$p_hash', salt='$salt' WHERE username='$log_username' LIMIT 1";
     $query = mysqli_query($db_conx, $sql);
 		mysqli_close($db_conx);
 		header("location: ../login.php");
